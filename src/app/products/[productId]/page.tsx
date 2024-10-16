6c23b23e26f4b50635957dd491a75fd7a6e9c838
oshiogifted <oshiogifted2011@gmail.com>
2024-10-15 23:53:24 -0400

    added card

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 export default function ProductDetails({
-    params,
-    searchParams,
-  }: {
-    params: { productId: string }
-    searchParams: { [key: string]: string | string[] | undefined }
-  }) {
-    return <h1>Details about product{params.productId}</h1>
-  }
+  params,
+  searchParams,
+}: {
+  params: { productId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  return (
+    <div className='grid grid-cols-3 gap-8'>
+      <h1>Details about product{params.productId}</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
